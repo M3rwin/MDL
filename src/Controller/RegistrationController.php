57544fe7dfa1a19719949931ca7626eae3d45cae
MatDoney <matthis.doney@gmail.com>
2024-03-25 09:33:17 +0100

    Changement de brevo

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -50,7 +50,7 @@ class RegistrationController extends AbstractController {
                 // generate a signed url and email it to the user
                 $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                         (new TemplatedEmail())
-                                ->from(new Address('ne-pas-repondre@mdl.fr', 'Maison Des Ligues'))
+                                ->from(new Address('ne-pas-repondre@doney.fr', 'Maison Des Ligues'))
                                 ->to($user->getEmail())
                                 ->subject('Please Confirm your Email')
                                 ->htmlTemplate('registration/confirmation_email.html.twig')
