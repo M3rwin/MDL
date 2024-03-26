7310ce59ad7aae227eb28ab79d7849ff2f1d45d1
marco.clin <marco.clin@CP-SIO-W11-PG10.bts.sio>
2024-03-26 11:56:21 +0100

    début traitage des données des forms

--- src/Controller/AjoutController.php
+++ src/Controller/AjoutController.php
@@ -16,7 +16,7 @@ class AjoutController extends AbstractController
     #[Route('/ajout', name: 'app_ajout')]
     public function index(Request $request): Response
     {
-        
+        // on initie/récupère les forms
         $formAtelier = $this->createForm(AjoutAtelierType::class);
         $formTheme = $this->createForm(AjoutThemeType::class);
         $formVacation = $this->createForm(AjoutVacationType::class);
@@ -24,14 +24,28 @@ class AjoutController extends AbstractController
         $formTheme->handleRequest($request);
         $formVacation->handleRequest($request);
         
-        
-        
-    
+        // on commence à déclarer les éléments du context
         $context = [
             'formAtelier' => $formAtelier->createView(),
             'formTheme' => $formTheme->createView(),
             'formVacation' => $formVacation->createView(),
         ];
+        
+        $forms = array($formAtelier, $formTheme, $formVacation);
+        
+        // récupération du type d'entité à faire persister
+        $formType = $request->request->get('form_type');
+        
+        // on traite les data du form
+        foreach($forms as $form){
+            if($form->isSubmitted() && $form->isValid()){
+                $data=$form->getData();
+                $context['data'] = $data;
+            }
+        }
+        
+    
+        
         
         return $this->render('ajout/index.html.twig', $context);
     }
