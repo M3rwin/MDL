85efc17af9fd5f72332713a69b10bd7616498192
MatDoney <matthis.doney@gmail.com>
2024-04-03 15:48:19 +0200

    creation page inscription congres + display de l'utilisateur

---
+++ src/Controller/InscriptionCongresController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Outils\Outils;
+use App\Entity\Compte;
+use App\Repository\CompteRepository;
+
+class InscriptionCongresController extends AbstractController
+{
+    #[Route('/inscriptioncongres', name: 'app_inscription_congres')]
+    public function index(CompteRepository $compte): Response
+    {
+        ///recupere le licencie
+        $user = $this->getUser();
+        $username = $user->getUserIdentifier();
+        $compte = $compte->findOneBy(['email'=>$username]);
+        $licencie = Outils::GetLicencieByNumLicence($compte->getNumlicence());
+        return $this->render('inscription_congres/index.html.twig', [
+            'controller_name' => 'InscriptionCongresController',
+            'user' => $user,
+            'licencie' => $licencie,
+        ]);
+    }
+}
