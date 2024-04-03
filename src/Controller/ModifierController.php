f8fde1c250348bf6679fa102e14f87efbd68c29b
marco.clin <marco.clin@CP-SIO-W11-PG10.bts.sio>
2024-04-03 13:07:57 +0200

    début ajout fonction modifier

--- src/Controller/ModifierController.php
+++ src/Controller/ModifierController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\AtelierRepository;
+use App\Repository\VacationRepository;
+use App\Form\AjoutVacationType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ModifierController extends AbstractController
 {
@@ -15,8 +19,30 @@ class ModifierController extends AbstractController
         $ateliers = $repo->findAll();
         
         return $this->render('modifier/index.html.twig', [
-            'controller_name' => 'ModifierController',
             'ateliers' => $ateliers,
+        ]);
+    }
+    
+    #[Route('/modifier/{id}', name: 'app_modifier_vacation')]
+    public function modifier(Request $request, VacationRepository $repo, EntityManagerInterface $em, int $id): Response
+    {
+        $vacation = $repo->find($id);
+        
+        $formVacation = $this->createForm(AjoutVacationType::class, $vacation);
+        $formVacation->handleRequest($request);
+        
+        if ($formVacation->isSubmitted() && $formVacation->isValid()) {
+                    $data = $formVacation->getData();
+                    $vacation->setDateheuredebut($data->dateheuredebut);
+                    $vacation->setDateheurefin($data->dateheurefin);
+                    $vacation->setAtelier($data->atelier);
+                    $em->flush();
+        }
+        
+        return $this->render('modifier/modifierVacation.html.twig', [
+            'formVacation' => $formVacation->createView(),
+            'vacation' => $vacation,
+            'id' => $id,
         ]);
     }
 }
