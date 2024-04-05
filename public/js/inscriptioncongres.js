d10594918acac01c5f4141de370215b8b7134449
MatthisDoney <matthis.doney@outlook.fr>
2024-04-05 11:23:32 +0200

    demande de confirmation amelioré

--- public/js/inscriptioncongres.js
+++ public/js/inscriptioncongres.js
@@ -1,9 +1,30 @@
 
 
 const confirmemail = document.getElementById("mailconfirm");
+const form = document.getElementById("formemail");
 
-confirmemail.addEventListener("click",function() {
-   window.confirm("Vous êtes sur le point de definir cet email comme mail de connexion"); 
+async function sendData() {
+  // Associate the FormData object with the form element
+  const formData = new FormData(form);
+
+  try {
+    const response = await fetch("/changementEmail", {
+      method: "POST",
+      // Defini formData en post
+      body: formData
+    });
+    console.log(await response);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+confirmemail.addEventListener("click",function(e) {
+    e.preventDefault();
+   if(window.confirm("Vous êtes sur le point de definir cet email comme mail de connexion")){
+       sendData();
+       location.reload();
+   }
    
 });
 
