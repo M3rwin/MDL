f82df7f4fc39870624eed60f41fe32b146981944
MatthisDoney <matthis.doney@outlook.fr>
2024-04-05 10:33:04 +0200

    demande de confirmation

---
+++ public/js/inscriptioncongres.js
@@ -0,0 +1,9 @@
+
+
+const confirmemail = document.getElementById("mailconfirm");
+
+confirmemail.addEventListener("click",function() {
+   window.confirm("Vous êtes sur le point de definir cet email comme mail de connexion"); 
+   
+});
+
