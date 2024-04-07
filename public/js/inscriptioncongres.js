b1c1db30afb8ba1878550c392db4ee8a982c6635
MatthisDoney <matthis.doney@outlook.fr>
2024-04-07 11:30:10 +0200

    Avancement dans la fiche de recap de l'inscription + modif sql

--- public/js/inscriptioncongres.js
+++ public/js/inscriptioncongres.js
@@ -13,18 +13,20 @@ const recap = document.getElementById("recap");
 const commit = document.getElementById("commit");
 //section conteant les bouton
 const SubmitSection = document.getElementById("SubmitSection");
-
-
+// Les Ateliers
+const ateliers = document.getElementsByName("ateliers[]");
+//Les nuités
+const nuites = document.getElementsByName("Nuites[]");
 //envoie les param du form en post
-async function sendData() {
+async function sendData(form) {
     // Associate the FormData object with the form element
-    const formDataMail = new FormData(formEmail);
+    const formData = new FormData(form);
 
     try {
         const response = await fetch("/changementEmail", {
             method: "POST",
             // Defini formData en post
-            body: formDataMail
+            body: formData
         });
         console.log(await response);
     } catch (e) {
@@ -35,33 +37,78 @@ async function sendData() {
 //recupère les informations du form
 recap.addEventListener("click", function () {
     const formDataInscription = new FormData(FormInscription);
+
     let keys = [];
-    
+
     for (const pair of formDataInscription.entries()) {
         keys = keys + [pair[0]];
     }
-    if(!keys.includes("ateliers[]")) {
+    //verifie si un atelier est coché
+    if (!keys.includes("ateliers[]")) {
         window.alert("Selectionnez un atelier");
-    }else {
+    } else {
+        //on prends la liste des ateliers cochés
+        let ateliersChecked = [];
+        for (let atelier of ateliers) {
+            if (atelier.checked) {
+                ateliersChecked.push(atelier.id);
+            }
+        }
+        //on prends la liste des nuités cochés
+        let nuitesChecked = [];
+        for (let nuite of nuites) {
+            if (nuite.checked) {
+                nuitesChecked.push(nuite.value.split('_'));
+            }
+        }
+
         FormDiv.style.display = "none";
         recap.style.display = "none";
         commit.style.display = "block";
+        FormInscription.innerHTML += '<div class="card-container" >'
+                + '<div class="card-header"><h2>Resumé de l\'inscription : </h2></div>'
+                + '<ul class="card-content" id="resume">'
+                + '<li><strong>Ateliers Selectionnés</strong></li>';
+        let resume = document.getElementById("resume");
+
+        for (let atelier of ateliersChecked) {
+
+            resume.innerHTML += '<li>' + atelier + '</li>';
+        }
+        
+        let total =130;
+            resume.innerHTML += '<li><strong>Nuités Selectionnés</strong></li>';
+            for (let nuite of nuitesChecked) {
+
+                resume.innerHTML += '<li>' + nuite[1] +' <strong>'+nuite[2]+'</strong> '+nuite[4] +'<strong style="display: flex;flex-direction: row-reverse;"> '+nuite[3]+ '€</strong></li>';
+                total += parseInt(nuite[3]);
+            }
+            resume.innerHTML +='<li>INSCRIPTION AU CONGRES'+'<strong style="display: flex;flex-direction: row-reverse;"> '+130+ '€</strong></li>'
+                    +'<h2> TOTAL : '+total +'€</h2>';
+        FormInscription.innerHTML += '</ul><\div>';
     }
-    
+
     //debug a retiré
     for (const pair of formDataInscription.entries()) {
         console.log(pair[0], pair[1]);
     }
+    for (let atelier of ateliers) {
+        if (atelier.checked) {
+            console.log(atelier.id);
+        }
+    }
 
-   
+
 
 });
+
+
 //au moment de la validation
 confirmemail.addEventListener("click", function (e) {
     //empeche le submit automatique
     e.preventDefault();
     if (window.confirm("Vous êtes sur le point de definir cet email comme mail de connexion")) {
-        sendData().await;
+        sendData(formEmail).await;
         location.reload();
     }
 
--- sql/script parametre 2024.sql
+++ sql/script parametre 2024.sql
@@ -1,8 +1,13 @@
+-- creation hotels et chambres
 insert into hotel (id,nom,adresse1,cp,ville,tel,mail) values
-(1,"ibis Styles Lille Centre Gare Beffroi","172 rue Pierre Mauroy","59000","Lille","+33 1 87 21 37 10","mail"),
+(1,"Ibis Styles Lille Centre Gare Beffroi","172 rue Pierre Mauroy","59000","Lille","+33 1 87 21 37 10","mail"),
 (2,"Ibis Budget Lille Gares Vieux-Lille","10, Rue de Courtrai","59000","Lille","+33 1 87 21 37 10","mail");
 insert into categoriechambre (id,libellecategorie) values (1,"Single"),(2,"Double");
 insert into proposer (categoriechambre_id,hotel_id,tarifnuite) values (1,1,95),(1,2,105),(2,1,70),(2,2,80);
+
+-- creation restaurations
+
+-- creation comptes de test
 insert into compte (email,roles,password,numlicence,is_verified) values 
 ('admin@mdl.doney.fr','["ROLE_ADMIN"]','$2y$13$KDureIQswSiPP83jX9ezbu/2ER5o2uTACndetzGBfhl3OjR7inmbe','16620515736','1'),
 ('inscrit@mdl.doney.fr','["ROLE_INSCRIT"]','$2y$13$W6A4yw40TVYWGgiEM2LxhuMNQUQGLNf2AeWF7ln0Q3yvUYiyZVNP6','16660412463','1');
